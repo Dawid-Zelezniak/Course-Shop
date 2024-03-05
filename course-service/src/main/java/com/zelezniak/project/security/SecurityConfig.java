da9c46f354b98c798fdcc236ba1a2c7d56bbb462
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-05 13:40:22 +0100

    fix: depracated code in security class updated

--- course-service/src/main/java/com/zelezniak/project/security/SecurityConfig.java
+++ course-service/src/main/java/com/zelezniak/project/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -40,8 +41,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
                                                    AuthenticationSuccessHandler successHandler) throws Exception {
-        httpSecurity.csrf().disable();
-        httpSecurity.authorizeRequests((configurer) -> {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((configurer) -> {
                     configurer
                             .requestMatchers("/registration/**").permitAll()
                             .requestMatchers("/login/**").permitAll()
