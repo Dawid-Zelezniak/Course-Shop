b0f846e4c43b8938078b12e6cd8814645931895f
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-06 13:08:55 +0100

    new package : role

---
+++ course-service/src/main/java/com/zelezniak/project/role/Role.java
@@ -0,0 +1,28 @@
+package com.zelezniak.project.role;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
