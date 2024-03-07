8cb6b5f08c3ead4d68171d668ec352c7527e6803
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-07 12:25:12 +0100

    new inner class : EmailInfoBuilder

--- course-service/src/main/java/com/zelezniak/project/dto/EmailInfo.java
+++ course-service/src/main/java/com/zelezniak/project/dto/EmailInfo.java
@@ -1,5 +1,8 @@
 package com.zelezniak.project.dto;
 
+import com.zelezniak.project.author.CourseAuthor;
+import com.zelezniak.project.course.Course;
+import com.zelezniak.project.student.Student;
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,4 +18,29 @@ public class EmailInfo implements Serializable {
     private String title;
     private Double price;
     private String orderId;
+
+    public static final class EmailInfoBuilder{
+
+        public static EmailInfo buildEmailInfo(Course course, CourseAuthor author, String orderId) {
+            return EmailInfo.builder()
+                    .email(author.getEmail())
+                    .title(course.getTitle())
+                    .price(course.getPrice())
+                    .firstName(author.getFirstName())
+                    .lastName(author.getLastName())
+                    .orderId(orderId)
+                    .build();
+        }
+
+        public static EmailInfo buildEmailInfo(Course course, Student student, String orderId) {
+            return EmailInfo.builder()
+                    .email(student.getEmail())
+                    .title(course.getTitle())
+                    .price(course.getPrice())
+                    .firstName(student.getFirstName())
+                    .lastName(student.getLastName())
+                    .orderId(orderId)
+                    .build();
+        }
+    }
 }
