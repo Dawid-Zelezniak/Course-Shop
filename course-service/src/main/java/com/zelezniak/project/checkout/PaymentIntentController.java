ea0b93f91f6425e0cab074988714befe6b29b904
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-07 12:18:12 +0100

    @Slf4j has been removed

--- course-service/src/main/java/com/zelezniak/project/checkout/PaymentIntentController.java
+++ course-service/src/main/java/com/zelezniak/project/checkout/PaymentIntentController.java
@@ -5,14 +5,12 @@ import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
 import com.zelezniak.project.dto.PaymentInfo;
 import com.zelezniak.project.dto.PaymentIntentResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 public class PaymentIntentController {
 
     @PostMapping("/payment/intent")
