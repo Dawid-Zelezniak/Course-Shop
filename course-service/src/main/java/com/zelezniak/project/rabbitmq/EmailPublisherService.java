8d7f41f02525eab15b447aae8009a5e855af1f8c
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-07 12:30:07 +0100

    refactor:method name has been changed in EmailPublisherService

--- course-service/src/main/java/com/zelezniak/project/rabbitmq/EmailPublisherService.java
+++ course-service/src/main/java/com/zelezniak/project/rabbitmq/EmailPublisherService.java
@@ -4,9 +4,9 @@ import com.zelezniak.project.course.Course;
 import com.zelezniak.project.author.CourseAuthor;
 import com.zelezniak.project.student.Student;
 
-public interface EmailInfoSender {
+public interface EmailPublisherService {
 
-    void prepareEmailInfo(Course course, CourseAuthor author, String orderId);
+    void prepareAndSendEmailInfo(Course course, CourseAuthor author, String orderId);
 
-    void prepareEmailInfo(Course course, Student student,String orderId);
+    void prepareAndSendEmailInfo(Course course, Student student, String orderId);
 }
