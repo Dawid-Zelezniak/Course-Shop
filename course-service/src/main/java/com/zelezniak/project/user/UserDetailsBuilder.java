7fad7368a6a2c47f34261a49e9e9456448a64794
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-07 12:33:53 +0100

    UserDetailsBuilder is package-private now

--- course-service/src/main/java/com/zelezniak/project/user/UserDetailsBuilder.java
+++ course-service/src/main/java/com/zelezniak/project/user/UserDetailsBuilder.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-public class UserDetailsBuilder {
+class UserDetailsBuilder {
 
     public static UserDetails buildUserDetails(CourseAuthor author) {
         Collection<SimpleGrantedAuthority> authorities = author.getRoles()
