e4dc21f354b1a0a956fe535c9cb6d9e385b5481e
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-08 08:31:33 +0100

    class name changed to DataInitialization

--- course-service/src/main/java/com/zelezniak/project/initialization/DataInitialization.java
+++ course-service/src/main/java/com/zelezniak/project/initialization/DataInitialization.java
@@ -15,7 +15,7 @@ import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
-final class DataInitializationService {
+final class DataInitialization {
 
     private final RoleRepository roleRepository;
     private final CourseAuthorRepository authorRepository;
