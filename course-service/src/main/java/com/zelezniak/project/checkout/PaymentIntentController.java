9eae69acc7ca443d5e820ecaacc7884c5813c1a1
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-14 14:29:32 +0100

    PaymentIntentController is package private and final now

--- course-service/src/main/java/com/zelezniak/project/checkout/PaymentIntentController.java
+++ course-service/src/main/java/com/zelezniak/project/checkout/PaymentIntentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PaymentIntentController {
+final class PaymentIntentController {
 
     @PostMapping("/payment/intent")
     @ResponseBody
