5f7f6f3bcf7e1bda9b1986580bc16c69c86fd512
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-14 15:12:53 +0100

    GlobalExceptionhandler is final and package private now

--- email-service/src/main/java/com/zelezniak/emailservice/exception/GlobalExceptionHandler.java
+++ email-service/src/main/java/com/zelezniak/emailservice/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+final class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailException.class)
     public ResponseEntity<ErrorInfo> courseExceptionHandler(EmailException exception) {
