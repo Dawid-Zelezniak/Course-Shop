77e0e02dcc3f4218a74f3dc12897686c8c9216dd
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-19 09:46:58 +0100

    new views and attributes has been added

--- course-service/src/main/java/com/zelezniak/project/common/AttributesAndTemplatesNames.java
+++ course-service/src/main/java/com/zelezniak/project/common/AttributesAndTemplatesNames.java
@@ -2,39 +2,44 @@ package com.zelezniak.project.common;
 
 public interface AttributesAndTemplatesNames {
 
-            ///////////VIEWS//////////
-    String CHECKOUT_VIEW = "checkout-form";
-    String PAYMENT_SUCCESS_VIEW = "payment-success";
-    String COURSES_LIST_VIEW = "courses";
-    String REGISTRATION_VIEW = "register/registration-form";
-    String STUDENTS_LIST_VIEW = "students";
-    String AUTHORS_LIST_VIEW = "authors";
+    ////////////ATTRIBUTES///////////
+    String AMOUNT_ATTRIBUTE = "amount";
+    String AUTHOR_ATTRIBUTE = "author";
+    String AUTHOR_ID_ATTRIBUTE = "authorId";
+    String AUTHORS_ATTRIBUTE = "authors";
+    String BOUGHT_COURSES_ATTRIBUTE = "boughtCourses";
+    String CATEGORIES_ATTRIBUTE = "categories";
+    String COURSE_ATTRIBUTE = "course";
+    String COURSES_ATTRIBUTE = "courses";
+    String EMAIL_ATTRIBUTE = "email";
+    String ERROR_INFO_ATTRIBUTE = "errorInfo";
+    String INFO_ATTRIBUTE = "info";
+    String PARTICIPANTS_ATTRIBUTE = "participants";
+    String PAYMENT_INFO_ATTRIBUTE = "paymentInfo";
+    String PRODUCT_NAME_ATTRIBUTE = "productName";
+    String PUBLIC_KEY_ATTRIBUTE = "publicKey";
+    String STUDENTS_ATTRIBUTE = "students";
+    String STUDENT_ATTRIBUTE = "student";
+    String TOTAL_PRICE_ATTRIBUTE = "totalPrice";
+    String WEB_USER_ATTRIBUTE = "webUser";
+    String ORDERS_ATTRIBUTE = "orders";
+
+    ///////////VIEWS//////////
     String ACCESS_DENIED_VIEW = "access-denied";
+    String ADD_COURSES_VIEW = "add-courses-form";
+    String AUTHORS_LIST_VIEW = "authors";
+    String AUTHOR_DETAILS_VIEW = "author-details";
+    String CHECKOUT_VIEW = "checkout-form";
+    String COURSE_DETAILS_VIEW = "course-details";
+    String COURSES_LIST_VIEW = "courses";
+    String CREATED_BY_AUTHOR_VIEW = "courses-created-by-author";
     String LOGIN_VIEW = "login";
     String PAYMENT_INFORMATION_VIEW = "payment-information";
-    String ADD_COURSES_VIEW = "add-courses-form";
+    String PAYMENT_SUCCESS_VIEW = "payment-success";
+    String REGISTRATION_VIEW = "register/registration-form";
+    String STUDENT_DETAILS_VIEW = "student-details";
+    String STUDENTS_LIST_VIEW = "students";
     String UPDATE_COURSE_VIEW = "update-course";
-    String COURSE_DETAILS_VIEW = "course-details";
-    String CREATED_BY_AUTHOR_VIEW = "courses-created-by-author";
     String USER_COURSES_VIEW = "user-courses";
     String USER_ORDERS_VIEW = "user-orders";
-
-            ////////////ATTRIBUTES///////////
-    String COURSES_ATTRIBUTE = "courses";
-    String COURSE_ATTRIBUTE = "course";
-    String INFO_ATTRIBUTE = "info";
-    String PAYMENT_INFO_ATTRIBUTE = "paymentInfo";
-    String ERROR_INFO_ATTRIBUTE = "errorInfo";
-    String STUDENTS_ATTRIBUTE = "students";
-    String PARTICIPANTS_ATTRIBUTE = "participants";
-    String AUTHORS_ATTRIBUTE = "authors";
-    String AUTHOR_ID_ATTRIBUTE = "authorId";
-    String WEB_USER_ATTRIBUTE = "webUser";
-    String TOTAL_PRICE_ATTRIBUTE = "totalPrice";
-    String ORDERS_ATTRIBUTE = "orders";
-    String PUBLIC_KEY_ATTRIBUTE = "publicKey";
-    String AMOUNT_ATTRIBUTE = "amount";
-    String EMAIL_ATTRIBUTE = "email";
-    String PRODUCT_NAME_ATTRIBUTE = "productName";
-    String CATEGORIES_ATTRIBUTE = "categories";
 }
