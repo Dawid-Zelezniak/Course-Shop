76109c2650f12d6d0b671f05966b2a1112414bfb
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-03-19 09:55:15 +0100

    new methods: authorDetails,studentDetails in UserController

--- course-service/src/main/java/com/zelezniak/project/user/UserController.java
+++ course-service/src/main/java/com/zelezniak/project/user/UserController.java
@@ -3,14 +3,18 @@ package com.zelezniak.project.user;
 
 import com.zelezniak.project.author.AuthorService;
 import com.zelezniak.project.author.CourseAuthor;
+import com.zelezniak.project.course.Course;
+import com.zelezniak.project.order.Order;
 import com.zelezniak.project.student.Student;
 import com.zelezniak.project.student.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Set;
 
 import static com.zelezniak.project.common.AttributesAndTemplatesNames.*;
 
@@ -34,6 +38,32 @@ public final class UserController {
         ModelAndView modelAndView = new ModelAndView(AUTHORS_LIST_VIEW);
         List<CourseAuthor> allAuthors = authorService.getAllAuthors();
         modelAndView.addObject(AUTHORS_ATTRIBUTE, allAuthors);
+        return modelAndView;
+    }
+
+    @GetMapping("/author/details")
+    public ModelAndView authorDetails(@RequestParam Long authorId) {
+        ModelAndView modelAndView = new ModelAndView(AUTHOR_DETAILS_VIEW);
+        CourseAuthor author = authorService.getById(authorId);
+        Set<Course> createdByAuthor = author.getCreatedByAuthor();
+        Set<Course> boughtCourses = author.getBoughtCourses();
+        Set<Order> authorOrders = author.getAuthorOrders();
+        modelAndView.addObject(AUTHOR_ATTRIBUTE, author);
+        modelAndView.addObject(COURSES_ATTRIBUTE,createdByAuthor);
+        modelAndView.addObject(BOUGHT_COURSES_ATTRIBUTE,boughtCourses);
+        modelAndView.addObject(ORDERS_ATTRIBUTE,authorOrders);
+        return modelAndView;
+    }
+
+    @GetMapping("/student/details")
+    public ModelAndView studentDetails(@RequestParam Long studentId) {
+        ModelAndView modelAndView = new ModelAndView(STUDENT_DETAILS_VIEW);
+        Student student = studentService.findById(studentId);
+        Set<Course> boughtCourses = student.getBoughtCourses();
+        Set<Order> studentOrders = student.getStudentOrders();
+        modelAndView.addObject(BOUGHT_COURSES_ATTRIBUTE,boughtCourses);
+        modelAndView.addObject(ORDERS_ATTRIBUTE,studentOrders);
+        modelAndView.addObject(STUDENT_ATTRIBUTE,student);
         return modelAndView;
     }
 }
