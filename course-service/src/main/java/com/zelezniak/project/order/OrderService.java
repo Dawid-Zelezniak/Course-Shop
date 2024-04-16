9ed01bf5b188cc6124232a04217879f277694e7e
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-04-16 14:31:21 +0200

    refactor in OrderService:method totalOrdersPrice uses Money value object now

--- course-service/src/main/java/com/zelezniak/project/order/OrderService.java
+++ course-service/src/main/java/com/zelezniak/project/order/OrderService.java
@@ -4,14 +4,15 @@ import com.zelezniak.project.author.CourseAuthor;
 import com.zelezniak.project.course.Course;
 import com.zelezniak.project.student.Student;
 
+import java.math.BigDecimal;
 import java.util.Set;
 
 public interface OrderService {
 
-    static double totalOrdersPrice(Set<Order> orders) {
+    static BigDecimal totalOrdersPrice(Set<Order> orders) {
         return orders.stream()
-                .map(Order::getTotalPrice)
-                .reduce(0.0, Double::sum);
+                .map(order -> order.getCoursePrice().getMoney())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     Order createOrder(Course course, CourseAuthor author);
