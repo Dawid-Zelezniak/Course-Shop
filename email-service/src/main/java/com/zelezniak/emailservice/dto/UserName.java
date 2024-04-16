81f4ba66060236e5a2ed0dd7269ef0c596a627e3
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-04-16 15:11:27 +0200

    new dto: UserName

---
+++ email-service/src/main/java/com/zelezniak/emailservice/dto/UserName.java
@@ -0,0 +1,13 @@
+package com.zelezniak.emailservice.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserName {
+
+    private  String firstName;
+    private  String lastName;
+
+    public UserName() {
+    }
+}
