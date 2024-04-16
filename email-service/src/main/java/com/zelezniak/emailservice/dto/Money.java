e312dbb5ade29b9b764c99a1c47084543a135faa
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-04-16 15:11:02 +0200

    new dto : Money

---
+++ email-service/src/main/java/com/zelezniak/emailservice/dto/Money.java
@@ -0,0 +1,14 @@
+package com.zelezniak.emailservice.dto;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class Money {
+    private  BigDecimal money;
+
+    public Money() {
+    }
+
+}
