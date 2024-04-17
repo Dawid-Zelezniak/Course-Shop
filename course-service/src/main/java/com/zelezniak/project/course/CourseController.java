171ed84ff3d1070ef385ddbb464f8a45c9c6d6cf
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-04-17 10:38:54 +0200

    refactor in CourseController : logic has been moved to CourseViewServiceImpl

--- course-service/src/main/java/com/zelezniak/project/course/CourseController.java
+++ course-service/src/main/java/com/zelezniak/project/course/CourseController.java
@@ -1,19 +1,8 @@
 package com.zelezniak.project.course;
 
 
-import com.zelezniak.project.author.AuthorService;
-import com.zelezniak.project.author.CourseAuthor;
-import com.zelezniak.project.common.FormErrorHandler;
-import com.zelezniak.project.dto.PaymentInfo;
-import com.zelezniak.project.exception.CourseException;
-import com.zelezniak.project.exception.ErrorInfo;
-import com.zelezniak.project.exception.UserException;
-import com.zelezniak.project.student.Student;
-import com.zelezniak.project.student.StudentService;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
-import java.util.List;
-import java.util.Set;
 
 import static com.zelezniak.project.common.AttributesAndTemplatesNames.*;
 
@@ -32,111 +19,53 @@ import static com.zelezniak.project.common.AttributesAndTemplatesNames.*;
 @RequiredArgsConstructor
 public final class CourseController {
 
-    private final HttpSession httpSession;
-    private final CourseService courseService;
-    private final AuthorService authorService;
-    private final StudentService studentService;
-    @Value("${categories}")
-    private List<String>categories;
+    private final CourseViewService courseViewService;
 
     @GetMapping("/courses")
     public ModelAndView availableCourses(Principal principal) {
-        ModelAndView modelAndView = new ModelAndView(COURSES_LIST_VIEW);
-        List<Course> courses = courseService.getAllAvailableCourses(principal.getName());
-        modelAndView.addObject(COURSES_ATTRIBUTE, courses);
-        return modelAndView;
+        return courseViewService.availableCoursesView(principal);
     }
 
     @GetMapping("/courses/payment/info")
     public ModelAndView prepareOrder(@RequestParam Long courseId, Principal principal) {
-        ModelAndView modelAndView = new ModelAndView(PAYMENT_INFORMATION_VIEW);
-        Course courseToBuy = courseService.findById(courseId);
-        PaymentInfo paymentInfo = PaymentInfo.PaymentInfoBuilder
-                .buildPaymentInfo(courseToBuy, principal.getName());
-        modelAndView.addObject(PAYMENT_INFO_ATTRIBUTE, paymentInfo);
-        return modelAndView;
+        return courseViewService.prepareOrderInfo(courseId, principal);
     }
 
     @GetMapping("/add/courses/form")
     public ModelAndView addCourseForm(Principal principal) {
-        ModelAndView modelAndView = new ModelAndView(ADD_COURSES_VIEW);
-        String email = principal.getName();
-        CourseAuthor authorFromDb = authorService.findByEmail(email);
-        httpSession.setAttribute(AUTHOR_ID_ATTRIBUTE,authorFromDb.getAuthorId());
-        modelAndView.addObject(COURSE_ATTRIBUTE, new Course());
-        modelAndView.addObject(CATEGORIES_ATTRIBUTE,categories);
-        return modelAndView;
+        return courseViewService.addCourseView(principal);
     }
 
     @PostMapping("/save/courses")
     public ModelAndView saveCourse(@ModelAttribute(COURSE_ATTRIBUTE) @Valid Course course,
                                    BindingResult bindingResult) {
-        Long authorId = (Long)httpSession.getAttribute(AUTHOR_ID_ATTRIBUTE);
-        ModelAndView modelAndView = new ModelAndView(ADD_COURSES_VIEW);
-        ModelAndView errors = FormErrorHandler.getErrors(bindingResult, modelAndView);
-        if (errors != null) {return errors;}
-        else {
-            try {
-                courseService.addCourse(course, authorId);
-                modelAndView.addObject(INFO_ATTRIBUTE, "New course has been created!");
-                return modelAndView;
-            } catch (CourseException ex) {
-                return modelAndView.addObject(
-                        ERROR_INFO_ATTRIBUTE, new ErrorInfo(ex.getCourseError().getMessage()));
-            }
-        }
+        return courseViewService.handleCourseSaving(bindingResult, course);
     }
 
     @GetMapping("/courses/details")
     public ModelAndView courseDetails(@RequestParam Long courseId) {
-        ModelAndView modelAndView = new ModelAndView(COURSE_DETAILS_VIEW);
-        Course course = courseService.findById(courseId);
-        int participantsNumber = course.countCourseParticipants();
-        modelAndView.addObject(COURSE_ATTRIBUTE, course);
-        modelAndView.addObject(PARTICIPANTS_ATTRIBUTE, participantsNumber);
-        return modelAndView;
+        return courseViewService.getCourseDetailsView(courseId);
     }
 
     @GetMapping("/update/courses")
     public ModelAndView courseUpdateForm(@RequestParam Long courseId) {
-        ModelAndView modelAndView = new ModelAndView(UPDATE_COURSE_VIEW);
-        Course course = courseService.findById(courseId);
-        modelAndView.addObject(COURSE_ATTRIBUTE, course);
-        return modelAndView;
+        return courseViewService.courseUpdateView(courseId);
     }
 
     @PostMapping("/update/courses/")
-    public ModelAndView updateCourse(@RequestParam Long courseId, @ModelAttribute(COURSE_ATTRIBUTE) @Valid Course course, BindingResult bindingResult) {
-        ModelAndView modelAndView = new ModelAndView(UPDATE_COURSE_VIEW);
-        ModelAndView errors = FormErrorHandler.getErrors(bindingResult, modelAndView);
-        if (errors != null) {return errors;}
-        else {
-            try {
-                courseService.updateCourse(courseId, course);
-                modelAndView.addObject(INFO_ATTRIBUTE, "course updated successfully");
-            } catch (CourseException ex) {
-                modelAndView.addObject(ERROR_INFO_ATTRIBUTE, new ErrorInfo(ex.getCourseError().getMessage()));
-            }return modelAndView;
-        }
+    public ModelAndView updateCourse(@RequestParam Long courseId,
+                                     @ModelAttribute(COURSE_ATTRIBUTE) @Valid Course course,
+                                     BindingResult bindingResult) {
+        return courseViewService.handleCourseUpdate(courseId, bindingResult, course);
     }
 
     @GetMapping("/purchased/courses")
     public ModelAndView coursesBoughtByUser(Principal principal) {
-        ModelAndView modelAndView = new ModelAndView(USER_COURSES_VIEW);
-        String email = principal.getName();
-        CourseAuthor author = authorService.findByEmail(email);
-        Student student = studentService.findByEmail(email);
-        Set<Course> userCourses = author != null ? author.getBoughtCourses() : student.getBoughtCourses();
-        modelAndView.addObject(COURSES_ATTRIBUTE, userCourses);
-        return modelAndView;
+        return courseViewService.coursesBoughtByUserView(principal);
     }
 
     @GetMapping("/author/courses")
     public ModelAndView coursesCreatedByAuthor(Principal principal) {
-        ModelAndView modelAndView = new ModelAndView(CREATED_BY_AUTHOR_VIEW);
-        String authorEmail = principal.getName();
-        Set<Course> createdByAuthor = authorService.findByEmail(authorEmail).getCreatedByAuthor();
-        modelAndView.addObject(COURSES_ATTRIBUTE, createdByAuthor);
-        return modelAndView;
+        return courseViewService.coursesCreatedByAuthorView(principal);
     }
 }
