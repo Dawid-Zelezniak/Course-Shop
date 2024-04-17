42270827e90fb10a56cce2f82035ddfd395f2927
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-04-17 10:37:31 +0200

    new classes: CourseViewService & CourseViewServiceImpl

---
+++ course-service/src/main/java/com/zelezniak/project/course/CourseViewService.java
@@ -0,0 +1,27 @@
+package com.zelezniak.project.course;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+
+public interface CourseViewService {
+
+    ModelAndView availableCoursesView(Principal principal);
+
+    ModelAndView prepareOrderInfo(Long courseId, Principal principal);
+
+    ModelAndView addCourseView(Principal principal);
+
+    ModelAndView handleCourseSaving(BindingResult bindingResult, Course course);
+
+    ModelAndView getCourseDetailsView(Long courseId);
+
+    ModelAndView courseUpdateView(Long courseId);
+
+    ModelAndView handleCourseUpdate(Long courseId, BindingResult bindingResult, Course course);
+
+    ModelAndView coursesBoughtByUserView(Principal principal);
+
+    ModelAndView coursesCreatedByAuthorView(Principal principal);
+}
