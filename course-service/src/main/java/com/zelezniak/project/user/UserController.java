5caf253cb84d67a477e8456f7a42e38ccbfc5a13
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-04-17 16:40:34 +0200

    UserController : logic has been moved to UserViewService

--- course-service/src/main/java/com/zelezniak/project/user/UserController.java
+++ course-service/src/main/java/com/zelezniak/project/user/UserController.java
@@ -1,69 +1,35 @@
 package com.zelezniak.project.user;
 
 
-import com.zelezniak.project.author.AuthorService;
-import com.zelezniak.project.author.CourseAuthor;
-import com.zelezniak.project.course.Course;
-import com.zelezniak.project.order.Order;
-import com.zelezniak.project.student.Student;
-import com.zelezniak.project.student.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-import java.util.Set;
-
-import static com.zelezniak.project.common.AttributesAndTemplatesNames.*;
-
 @Controller
 @RequiredArgsConstructor
 public final class UserController {
 
-    private final StudentService studentService;
-    private final AuthorService authorService;
+    private final UserView userView;
 
     @GetMapping("/all/students")
-    public ModelAndView allStudents() {
-        ModelAndView modelAndView = new ModelAndView(STUDENTS_LIST_VIEW);
-        List<Student> allStudents = studentService.getAllStudents();
-        modelAndView.addObject(STUDENTS_ATTRIBUTE, allStudents);
-        return modelAndView;
+    public ModelAndView showAllStudents() {
+      return userView.allStudentsView();
     }
 
     @GetMapping("/all/authors")
-    public ModelAndView allAuthors() {
-        ModelAndView modelAndView = new ModelAndView(AUTHORS_LIST_VIEW);
-        List<CourseAuthor> allAuthors = authorService.getAllAuthors();
-        modelAndView.addObject(AUTHORS_ATTRIBUTE, allAuthors);
-        return modelAndView;
+    public ModelAndView showAllAuthors() {
+        return userView.allAuthorsView();
     }
 
     @GetMapping("/author/details")
-    public ModelAndView authorDetails(@RequestParam Long authorId) {
-        ModelAndView modelAndView = new ModelAndView(AUTHOR_DETAILS_VIEW);
-        CourseAuthor author = authorService.findById(authorId);
-        Set<Course> createdByAuthor = author.getCreatedByAuthor();
-        Set<Course> boughtCourses = author.getBoughtCourses();
-        Set<Order> authorOrders = author.getAuthorOrders();
-        modelAndView.addObject(AUTHOR_ATTRIBUTE, author);
-        modelAndView.addObject(COURSES_ATTRIBUTE,createdByAuthor);
-        modelAndView.addObject(BOUGHT_COURSES_ATTRIBUTE,boughtCourses);
-        modelAndView.addObject(ORDERS_ATTRIBUTE,authorOrders);
-        return modelAndView;
+    public ModelAndView showAuthorDetails(@RequestParam Long authorId) {
+    return userView.authorDetailsView(authorId);
     }
 
     @GetMapping("/student/details")
-    public ModelAndView studentDetails(@RequestParam Long studentId) {
-        ModelAndView modelAndView = new ModelAndView(STUDENT_DETAILS_VIEW);
-        Student student = studentService.findById(studentId);
-        Set<Course> boughtCourses = student.getBoughtCourses();
-        Set<Order> studentOrders = student.getStudentOrders();
-        modelAndView.addObject(BOUGHT_COURSES_ATTRIBUTE,boughtCourses);
-        modelAndView.addObject(ORDERS_ATTRIBUTE,studentOrders);
-        modelAndView.addObject(STUDENT_ATTRIBUTE,student);
-        return modelAndView;
+    public ModelAndView showStudentDetails(@RequestParam Long studentId) {
+    return userView.studentDetailsView(studentId);
     }
 }
