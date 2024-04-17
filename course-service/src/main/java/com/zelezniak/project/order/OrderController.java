c391e67f31c0b475e899aed5a027df4bd582caf9
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-04-17 10:49:19 +0200

    Logic moved from OrderController to OrderViewService

--- course-service/src/main/java/com/zelezniak/project/order/OrderController.java
+++ course-service/src/main/java/com/zelezniak/project/order/OrderController.java
@@ -1,33 +1,20 @@
 package com.zelezniak.project.order;
 
-import com.zelezniak.project.author.AuthorService;
-import com.zelezniak.project.author.CourseAuthor;
-import com.zelezniak.project.student.Student;
-import com.zelezniak.project.student.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.math.BigDecimal;
 import java.security.Principal;
-import java.util.Set;
-
-import static com.zelezniak.project.common.AttributesAndTemplatesNames.*;
 
 @RequiredArgsConstructor
 @Controller
 public class OrderController {
 
- private final OrderService orderService;
+    private final OrderView orderView;
 
     @GetMapping({"/user/orders"})
     public ModelAndView userOrders(Principal principal) {
-        ModelAndView modelAndView = new ModelAndView(USER_ORDERS_VIEW);
-        Set<Order> ordersForUser = orderService.getOrdersForUser(principal);
-        BigDecimal totalPrice = OrderService.totalOrdersPrice(ordersForUser);
-        modelAndView.addObject(TOTAL_PRICE_ATTRIBUTE, totalPrice);
-        modelAndView.addObject(ORDERS_ATTRIBUTE, ordersForUser);
-        return modelAndView;
+        return orderView.getUserOrders(principal);
     }
 }
