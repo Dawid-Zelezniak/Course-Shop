278c5b887c02421411a0501f6a62974f7fbd39e4
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-04-21 09:37:38 +0200

    missing @Transactional applied in StudentServiceImpl

--- course-service/src/main/java/com/zelezniak/project/student/StudentServiceImpl.java
+++ course-service/src/main/java/com/zelezniak/project/student/StudentServiceImpl.java
@@ -5,32 +5,37 @@ import com.zelezniak.project.exception.UserError;
 import com.zelezniak.project.exception.UserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-final class StudentServiceImpl implements StudentService {
+class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
 
+    @Transactional(readOnly = true)
     public List<Student> getAllStudents() {
         return this.studentRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Student findByEmail(String email) {
         return studentRepository.findByUserCredentialsEmail(email);
     }
 
+    @Transactional
     public void saveStudent(Student student) {
         if(student !=null){studentRepository.save(student);}
     }
 
+    @Transactional
     public boolean existsByEmail(String email) {
         return studentRepository.existsByUserCredentialsEmail(email);
     }
 
-    @Override
+    @Transactional(readOnly = true)
     public Student findById(Long id) {
         return studentRepository.findById(id)
                 .orElseThrow(()-> new UserException(UserError.USER_NOT_FOUND));
